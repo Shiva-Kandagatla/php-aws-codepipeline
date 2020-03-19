559543c02434dccf6a7112690d65533cf289d0aa
Shiva_K <shiva.k@avplat.com>
2020-03-19 18:58:54 +0530

    changed scripts

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-apt-get update
-apt install apache2
-ufw allow in "Apache Full"
-sed -i  's/\<index.php\>/ /g'  /etc/apache2/mods-enabled/dir.conf
-sed -i  's/\<DirectoryIndex\>/& index.php/' /etc/apache2/mods-enabled/dir.conf
-apt-get install php7.2 -y
-apt-get install php7.2-{bcmath,bz2,intl,gd,mbstring,mysql,zip,fpm,soap,curl,json,xml} -y
-service php7.2-fpm reload
-service apache2 restart
-adduser ubuntu  www-data
+sudo apt-get update
+sudo apt install apache2
+sudo ufw allow in "Apache Full"
+sudo sed -i  's/\<index.php\>/ /g'  /etc/apache2/mods-enabled/dir.conf
+sudo sed -i  's/\<DirectoryIndex\>/& index.php/' /etc/apache2/mods-enabled/dir.conf
+sudo apt-get install php7.2 -y
+sudo apt-get install php7.2-{bcmath,bz2,intl,gd,mbstring,mysql,zip,fpm,soap,curl,json,xml} -y
+sudo service php7.2-fpm reload
+sudo service apache2 restart
+sudo adduser ubuntu  www-data
 
-cd /var/www/
-git clone https://gitlab.com/Shiva_K/php-aws-codepipeline.git
-chown -R $USER:$USER /var/www/php-aws-codepipeline
-chmod -R 755 /var/www
+# cd /var/www/
+# git clone https://gitlab.com/Shiva_K/php-aws-codepipeline.git
+# chown -R $USER:$USER /var/www/php-aws-codepipeline
+sudo chmod -R 755 /var/www
 
-touch /etc/apache2/sites-available/php-aws-codepipeline.conf 
-echo "<VirtualHost *:80>
+sudo touch /etc/apache2/sites-available/php-aws-codepipeline.conf 
+sudo echo "<VirtualHost *:80>
         ServerAdmin webmaster@localhost
         ServerName php-aws-codepipeline
         ServerAlias www.php-aws-codepipeline
@@ -24,7 +24,7 @@ echo "<VirtualHost *:80>
         ErrorLog \${APACHE_LOG_DIR}/php-aws-codepipeline-error.log
         CustomLog \${APACHE_LOG_DIR}/access.log combined
       </VirtualHost>" >  /etc/apache2/sites-available/php-aws-codepipeline.conf 
-a2ensite php-aws-codepipeline.conf
-a2dissite 000-default.conf
-apache2ctl configtest
-systemctl restart apache2
+sudo a2ensite php-aws-codepipeline.conf
+sudo a2dissite 000-default.conf
+sudo apache2ctl configtest
+sudo systemctl restart apache2
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-service apache2 stop
+sudo service apache2 stop
